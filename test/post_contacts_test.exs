838979cb4f2adb72e1b98884a6bb7fa761409113
nathaliaifurita <nathaliaofreire@gmail.com>
2021-08-17 22:40:47 -0300

    Adding new tests

--- test/post_contacts_test.exs
+++ test/post_contacts_test.exs
@@ -8,7 +8,7 @@ defmodule ApiAutomation.PostContactsTest do
         "name" => Faker.StarWars.character(),
         "last_name" => Faker.StarWars.planet(),
         "email" => Faker.Internet.email(),
-        "age" => "19",
+        "age" => Faker.random_between(1, 99),
         "phone" => Faker.Phone.EnUs.phone(),
         "address" => Faker.StarWars.planet() <> " Street",
         "state" => Faker.StarWars.planet(),
@@ -16,12 +16,109 @@ defmodule ApiAutomation.PostContactsTest do
       }
 
       headers = %{
-        "Content-Type" => "application/json", 
+        "Content-Type" => "application/json",
         "Accept" => "application/vnd.tasksmanager.v2"
       }
 
-      {:ok, %{body: response, status_code: 201}} = HTTPoison.post("https://api-de-tarefas.herokuapp.com/contacts", Jason.encode!(body), headers)
-      |> IO.inspect
+      {:ok, %{body: _response, status_code: 201}} =
+        HTTPoison.post(
+          "https://api-de-tarefas.herokuapp.com/contacts",
+          Jason.encode!(body),
+          headers
+        )
+    end
+
+    test "returns an invalid email" do
+      body = %{
+        "name" => Faker.StarWars.character(),
+        "last_name" => Faker.StarWars.planet(),
+        "email" => "test",
+        "age" => Faker.random_between(1, 99),
+        "phone" => Faker.Phone.EnUs.phone(),
+        "address" => Faker.StarWars.planet() <> " Street",
+        "state" => Faker.StarWars.planet(),
+        "city" => Faker.StarWars.planet()
+      }
+
+      headers = %{
+        "Content-Type" => "application/json",
+        "Accept" => "application/vnd.tasksmanager.v2"
+      }
+
+      {:ok, %{body: response, status_code: 422}} =
+        HTTPoison.post(
+          "https://api-de-tarefas.herokuapp.com/contacts",
+          Jason.encode!(body),
+          headers
+        )
+
+      %{"errors" => errors} = Jason.decode!(response)
+
+      Enum.map(errors, fn _contact ->
+        assert %{"email" => "não é válido"}
+      end)
+    end
+
+    test "returns an error for blank email" do
+      body = %{
+        "name" => Faker.StarWars.character(),
+        "last_name" => Faker.StarWars.planet(),
+        "email" => "",
+        "age" => Faker.random_between(1, 99),
+        "phone" => Faker.Phone.EnUs.phone(),
+        "address" => Faker.StarWars.planet() <> " Street",
+        "state" => Faker.StarWars.planet(),
+        "city" => Faker.StarWars.planet()
+      }
+
+      headers = %{
+        "Content-Type" => "application/json",
+        "Accept" => "application/vnd.tasksmanager.v2"
+      }
+
+      {:ok, %{body: response, status_code: 422}} =
+        HTTPoison.post(
+          "https://api-de-tarefas.herokuapp.com/contacts",
+          Jason.encode!(body),
+          headers
+        )
+
+      %{"errors" => errors} = Jason.decode!(response)
+
+      Enum.map(errors, fn _contact ->
+        assert %{"email" => "não pode ficar em branco"}
+      end)
+    end
+
+    test "returns an error for an already used email" do
+      body = %{
+        "name" => Faker.StarWars.character(),
+        "last_name" => Faker.StarWars.planet(),
+        "email" => "nat@gmail.com",
+        "age" => Faker.random_between(1, 99),
+        "phone" => Faker.Phone.EnUs.phone(),
+        "address" => Faker.StarWars.planet() <> " Street",
+        "state" => Faker.StarWars.planet(),
+        "city" => Faker.StarWars.planet()
+      }
+
+      headers = %{
+        "Content-Type" => "application/json",
+        "Accept" => "application/vnd.tasksmanager.v2"
+      }
+
+      {:ok, %{body: response, status_code: 422}} =
+        HTTPoison.post(
+          "https://api-de-tarefas.herokuapp.com/contacts",
+          Jason.encode!(body),
+          headers
+        )
+
+      %{"errors" => errors} = Jason.decode!(response)
+
+      Enum.map(errors, fn _contact ->
+        assert %{"email" => "já está em usoo"}
+      end)
     end
   end
 end
