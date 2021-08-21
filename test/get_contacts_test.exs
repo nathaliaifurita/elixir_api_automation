b590508b0f43412bea17eb75faf8427115e31152
nathaliaifurita <nathaliaofreire@gmail.com>
2021-08-21 11:24:45 -0300

    Removing comments

--- test/get_contacts_test.exs
+++ test/get_contacts_test.exs
@@ -7,12 +7,8 @@ defmodule ApiAutomation.GetContactsTest do
       {:ok, %{body: response, status_code: 200}} =
         HTTPoison.get("https://api-de-tarefas.herokuapp.com/contacts")
 
-      # comparação de valores entre a resposta do HTTPOison e o {:ok, ...}
-      # comparando os padrões em um mapa pra pegar o body e colocar na variavel response
-      # comparação do status_code que deve ser igual a 200
       %{"data" => data} = Jason.decode!(response)
-      # O decode transforma os parametros do mapa numa string
-      # Transformando o Jason num mapa e comparando os padrões de data pra jogar na variavel data
+
       Enum.each(data, fn contact ->
         assert %{
                  "attributes" => %{
@@ -29,11 +25,6 @@ defmodule ApiAutomation.GetContactsTest do
                  "type" => _
                } = contact
       end)
-
-      # Pra cada "data" que tiver no response, 
-      # eu testo a estrutura do mapa de resposta do contact,
-      # usando a função anonima pra comparar os parametros de 
-      # dentro do mapa
     end
   end
 end
